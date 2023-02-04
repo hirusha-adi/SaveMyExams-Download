3405549ad8a523186adf737bb92140cfe5b190de
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-04 13:37:44 +0530

    user can select options

--- file.py
+++ file.py
@@ -4,40 +4,97 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from termcolor import colored
 
-url = "https://www.savemyexams.co.uk/a-level/maths_pure-1/cie/20/revision-notes/1-algebra--functions/1-1-quadratics/1-1-1-expanding-brackets/"
-
 def printBanner():
     print(colored("""  __                     _                 
  (_   _.     _  |\/|    |_     _. ._ _   _ 
  __) (_| \/ (/_ |  | \/ |_ >< (_| | | | _> 
                      /                     
-          """, 'magenta'))
+          """, 'cyan'))
 
 def loadUrls():
     filename = os.path.join(os.getcwd(), "urls.txt")
     if not os.path.isfile(filename):
         print(colored("[-] './urls.txt' not found", 'red'))
         filename = input(colored("[?] Enter filename with URLs: ", 'yellow'))
-    with open(filename, 'r') as _file:
-        return _file.readlines()
+    try:
+        with open(filename, 'r') as _file:
+            return _file.readlines()
+    except Exception as e:
+        print(colored(f"[-] {e}" ,'red'))
 
-def saveImages(url, width, height):
-
+def loadOptions():
     options = Options()
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--headless")
-    options.add_argument("--disable-impl-side-painting")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-setuid-sandbox")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--disable-webgl")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--remote-debugging-port=0")
-    options.add_argument("--start-maximized")
+
+    ynheadless = input(colored('[?] Enable Headless Mode? (recommended) [Yes/no]: ', 'yellow'))
+    if ynheadless.strip().lower().startswith('y') or ynheadless.strip() == '':
+        options.add_argument("--headless")
+        print(colored(f"[+] Using Headless Mode", 'green'))
+    else:
+        print(colored(f"[+] NOT Using Headless Mode", 'green'))
+        
+    optionset = {
+        "--disable-extensions": {
+            "q": "Disable extensions?",
+            "ok": "[+] Added '--disable-extensions'"
+        },
+        "--disable-gpu": {
+            "q": "Disable GPU?",
+            "ok": "[+] Added '--disable-gpu'"
+        },
+        "--disable-infobars": {
+            "q": "Disable Info Bars?",
+            "ok": "[+] Added '--disable-infobars'"
+        },
+        "--no-sandbox": {
+            "q": "Disable Sandbox?",
+            "ok": "[+] Added '--no-sandbox'"
+        },
+        "--disable-setuid-sandbox": {
+            "q": "Disable Set UID Sandbox?",
+            "ok": "[+] Added '--disable-setuid-sandbox'"
+        },
+        "--disable-software-rasterizer": {
+            "q": "Disable Software Rasterizer?",
+            "ok": "[+] Added '--disable-software-rasterizer'"
+        },
+        "--disable-webgl": {
+            "q": "Disable WebGL?",
+            "ok": "[+] Added '--disable-webgl'"
+        },
+        "--ignore-certificate-errors": {
+            "q": "Ignore Certificate Errors?",
+            "ok": "[+] Added '--ignore-certificate-errors'"
+        },
+        "--remote-debugging-port=0": {
+            "q": "Set Remote Debugging Port to 0?",
+            "ok": "[+] Added '--remote-debugging-port=0'"
+        },
+        "--start-maximized": {
+            "q": "Start Maximized?",
+            "ok": "[+] Added '--start-maximized'"
+        }
+    }
+    
+    loaddefault = input(colored('[?] Load default settings? (recommended) [Yes/no]: ', 'yellow'))
+    loadall =  loaddefault.strip().lower().startswith("y") or loaddefault.strip() == ""
+    for opt, dval in optionset.items():
+        if loadall:
+            options.add_argument(opt)
+            print(colored(f"{dval['ok']}", 'green'))
+        else:
+            userinp = input(colored(f"[?] {dval['q']} [Yes/no]: ")).strip().lower()
+            if userinp.startswith('y') or userinp == "":
+                options.add_argument(opt)
+                print(colored(f"{dval['ok']}", 'green'))
+    
+    return options
+    
+def saveImages(url, width, height, options):
+    
+    print(colored(f"[+] Starting to save:\n\tURL: {url}\n\tWidth: {width}\n\tHeight: {height}", 'green'))
 
     driver = webdriver.Chrome(options=options)
+    
     driver.get(url)
 
     classes = [
@@ -58,7 +115,7 @@ def saveImages(url, width, height):
         except Exception as e:
             print(f"[ERROR]: {e}")
         
-    driver.set_window_size(screen_width, screen_height)
+    driver.set_window_size(width, height)
 
     current_page_title = driver.find_element(by=By.CLASS_NAME, value="resource-title").text
 
@@ -67,8 +124,6 @@ def saveImages(url, width, height):
     scroll_how_many_times = math.ceil(page_height / windowinnerHeight)
 
     for i in range(int(scroll_how_many_times)):
-        i += 1
-
         filename = f"{current_page_title}_{i}.png"
         driver.save_screenshot(filename)
         print(f"[+] Saved {filename}")
@@ -81,9 +136,11 @@ def saveImages(url, width, height):
 def run():
     printBanner()
     urls = loadUrls()
+    options = loadOptions()
+    
     for url in urls:
-        # saveImages(url=url, width=1920, height=1080)
-        print(url)
+        saveImages(url=url, width=1920, height=1080, options=options)
 
 if __name__ == "__main__":
-        run()
+    run()
+    
