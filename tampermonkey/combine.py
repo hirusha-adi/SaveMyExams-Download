a2ea14bf184cf4bc18ec719b7bdac8b6655ca8a6
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-03-05 12:25:11 +0530

    give more control to user

--- tampermonkey/combine.py
+++ tampermonkey/combine.py
@@ -1,20 +1,38 @@
 import os
 import PyPDF2
 
-pdfs = []
+current_folder = os.path.split(os.getcwd())[-1]
 
+pdfs = []
 pdfiles = []
-for file in os.listdir():
-    if file.endswith(".pdf") and not (file == "final.pdf"):
+for file in os.listdir(os.getcwd()):
+    if file.endswith(".pdf") and not (file == current_folder):
         pdfiles.append(file)
         print(file)
 
+if len(pdfiles) == 0:
+    print("[ERROR] No .pdf files in Current Working Directory")
+    input("Press [ENTER] to exit.")
+    exit()
+
+filename = ""
+ufname = input(f"[?] Save File Name <{current_folder}> [Yes/Custom]\n: ")
+if not (ufname == 'y' or ufname == 'Y' or ufname == 'Yes' or ufname == 'yes'):
+    ufname == filename
+
+if not filename.endswith('.pdf'):
+    filename += '.pdf'
+
 pdfMerge = PyPDF2.PdfMerger()
 
-for filename in pdfiles:
-    pdfFile = open(filename, 'rb')
+print("[+] Merging in the current order: ")
+count = 1
+for open_file in pdfiles:
+    print(f'[{count}] - {open_file}')
+    pdfFile = open(open_file, 'rb')
     pdfReader = PyPDF2.PdfReader(pdfFile)
     pdfMerge.append(pdfReader)
 
 pdfFile.close()
-pdfMerge.write('final.pdf')
+pdfMerge.write(filename)
+input("[+] Merged everything successfully!\nPress [ENTER] to exit.")
